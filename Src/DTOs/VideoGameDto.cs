5a11636b2dd6c9e9a5944a7c84c15976b26e6107
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-30 22:05:12 -0400

    feat: add auto mapper

--- Src/DTOs/VideoGameDto.cs
+++ Src/DTOs/VideoGameDto.cs
@@ -1,3 +1,12 @@
 namespace videogames_dotnet_api.Src.DTOs;
 
-public class VideoGameDto { }
+public class VideoGameDto
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string Platform { get; set; }
+    public required int Price { get; set; }
+    public required DateOnly ReleaseDate { get; set; }
+    public required string ImageUrl { get; set; }
+}
---
+++ Src/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,23 @@
+using AutoMapper;
+using videogames_dotnet_api.Src.DTOs;
+using videogames_dotnet_api.Src.Models;
+
+namespace videogames_dotnet_api.Src.Helpers;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        CreateMap<CreateVideoGameDto, VideoGame>()
+            .ForMember(
+                dest => dest.ImageUrl,
+                opt => opt.MapFrom((src, dest, destMember, context) => context.Items["Image"])
+            )
+            .ForMember(
+                dest => dest.PublicId,
+                opt => opt.MapFrom((src, dest, destMember, context) => context.Items["PublicId"])
+            );
+
+        CreateMap<VideoGame, VideoGameDto>();
+    }
+}
