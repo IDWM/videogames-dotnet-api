62716b2dcec52ba03c036aa996d80f83cd14b419
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-30 20:24:26 -0400

    fix: add description column and change data type of release date

--- Src/Models/VideoGame.cs
+++ Src/Models/VideoGame.cs
@@ -1,11 +1,13 @@
 namespace videogames_dotnet_api.Src.Models;
 
-public class VideoGame {
+public class VideoGame
+{
     public int Id { get; set; }
     public required string Name { get; set; }
+    public required string Description { get; set; }
     public required string Platform { get; set; }
     public required int Price { get; set; }
-    public required string ReleaseDate { get; set; }
+    public required DateOnly ReleaseDate { get; set; }
     public required string ImageUrl { get; set; }
     public required string PublicId { get; set; }
 }
